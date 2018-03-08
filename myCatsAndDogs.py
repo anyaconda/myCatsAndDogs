18fabae2a20d84679f3f1f7955db1d1b282f65e5
anyaconda <anya@uw.edu>
2018-03-08 15:42:45 -0800

    Plotting Cats and Dogs separately; running a)SVM and b)tree model; generating a decision tree diagram.

--- myCatsAndDogs.py
+++ myCatsAndDogs.py
@@ -4,12 +4,14 @@
 
 from __future__ import division
 import matplotlib.pyplot as plt
+from matplotlib.text import TextPath
 import numpy as np
 import pandas as pd
 
+
 ### Step 1. Data Prep
 # load data
-data = pd.read_csv('data/myCatsAndDogs.csv')
+data = pd.read_csv('data/myCatsAndDogs.csv') #'data/Iris_modified.csv'
 data['label']=data['label'].astype('category')
 
 # explore data
@@ -17,32 +19,67 @@ print(data.shape)
 print(data.dtypes)
 print(data.head())
 
-# visualize the data.
+# visualize the data
 x = data.food
 y = data.sleep
-#print(data['label'].cat.categories)
-c = data.label #type series
-colors = c.apply(lambda x: 'royalblue' if x=='cat' else 'springgreen')
-edgecolors = c.apply(lambda x: 'b' if x=='cat' else 'g')
+groups = data.groupby('label')
+colors = ['crimson', 'mediumblue']
 
-
-#ref matplotlib scatterplot https://matplotlib.org/gallery/shapes_and_collections/scatter.html#sphx-glr-gallery-shapes-and-collections-scatter-py
-###plt.rcParams.update(pd.tools.plotting.mpl_stylesheet)
-#ref scatter symbols https://matplotlib.org/gallery/lines_bars_and_markers/scatter_symbol.html#sphx-glr-gallery-lines-bars-and-markers-scatter-symbol-py
-#ref markers https://matplotlib.org/api/markers_api.html
-
-plt.figure(figsize=[6,6])
-plt.scatter(x,y, c=colors, edgecolors=edgecolors, alpha=.9, marker='o', s=100)
-
-plt.title("Cats and Dogs")
+#plot
+#src https://stackoverflow.com/questions/21654635/scatter-plots-in-pandas-pyplot-how-to-plot-by-category
+#plt.figure(figsize=[8,8])
+i = 0
+fig, ax = plt.subplots()
+for name, group in groups:
+    letter = group.isCat.unique()
+    letter = letter[0]
+    ax.scatter(group.food, group.sleep, c=colors[i],  marker=TextPath((0, 0), letter), s = 400, alpha =.8, label = name)
+    i+=1
+##ax.legend()
+plt.title("Cat?")
 plt.xlabel("Food (in grams)")
 plt.ylabel("Sleep (in hours)")
-legend1 = plt.Line2D([0,0],[0,1], color='royalblue', marker='o', linestyle='', markersize=5)
-legend2 = plt.Line2D([0,0],[0,1], color='springgreen', marker='o', linestyle='', markersize=5)
-plt.legend([legend1,legend2], c.unique())
 plt.show()
 
-### not used
-# c.astype('int16')
-# plt.xlim(0,1000)
-# didn't work markers = c.apply(lambda x: r'$Y$' if x=='cat' else r'$N$')
+
+### Step 2. Train
+X = data[['food','sleep']]
+y = data['label']
+#x_train, Y_train =
+sample = [[600,22.0]]
+
+# 2a. SVM Model
+from sklearn import svm
+clf1 = svm.SVC()
+clf1.fit(X,y)
+y_hat1 = clf1.predict(sample)
+print ("predict1", y_hat1 )
+
+# 2b. Tree Model
+from sklearn import tree
+from sklearn.tree import export_graphviz
+
+clf2 = tree.DecisionTreeClassifier()
+clf2 = clf2.fit(X, y)
+y_hat2 = clf2.predict(sample)
+print ("predict2", y_hat2)
+
+#graphviz
+classes = data.label.unique()
+features = X.columns
+#create decision tree
+dot_data = tree.export_graphviz(clf2, out_file='vis/cats_tree.dot', class_names=classes, feature_names=features, filled=True, rounded=True) #, class_names=c.unique()
+
+#src http://scikit-learn.org/stable/modules/tree.html
+# todo
+# import graphviz
+# graph = graphviz.Source(dot_data)
+# graph
+
+# # converting into the pdf file from CLI
+# #dot -Tpdf file_name.dot -o file_name.pdf
+# import os
+# export_graphviz(clf,  filled=True, rounded=True) #feature_names=iris.feature_names,
+# os.system('dot -Tpng vis\cats_tree.dot -o vis\cats_tree.png')
+# os.system('dot -Tpdf vis\cats_tree.dot -o vis\cats_tree.pdf')
+
